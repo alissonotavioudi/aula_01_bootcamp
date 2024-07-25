99aab7b1bee6463ca93990d37816164558bad9e3
Alisson <alissonotavio@outlook.com>
2024-07-24 22:48:54 -0300

    adicionando arquivo exemplo_00.py

---
+++ exemplo_00.py
@@ -0,0 +1,18 @@
+print("Hello world")
+
+print("Olá, " + input("Digite seu nome: ")+ "!")
+
+#Crie programa que usuário digita nome e retorna a quantidade de caracters
+print(len(input("Digite seu nome: ")))
+
+#Crie um programa que some dois números que estão no formato de texto
+print(int(input("Digite o primeiro número:")) + int(input("Digite o segundo número:")))
+
+numero = 3 #int
+numero_decimal = 4.78 #float
+nome_de_usuario = "Alisson" #string
+verdadeiro = True #bool
+
+print(type(numero))
+
+
